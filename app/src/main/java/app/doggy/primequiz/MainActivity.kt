b0e9d61006e7afbae352987672510621de250095
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-08 00:07:27 +0900

    レイアウト変更。メソッドの返り値をなくした。

--- app/src/main/java/app/doggy/primequiz/MainActivity.kt
+++ app/src/main/java/app/doggy/primequiz/MainActivity.kt
@@ -1,26 +1,33 @@
 package app.doggy.primequiz
 
-//関数内で使う変数をActivity内で宣言してもOKか。
-//makeQuestion()では戻り値を返すようにした方が良いか。
-//->関数内で変数を使うことになるので、こっちの方が良さそう？
-//makeQuestion()内に、問題作成とは直接関係ないTextViewに表示する処理も書いて良いか（読みやすいか）。
-
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val QUESTION_COUNT = 10
+    }
+
     //問題を格納する変数。
     var questionNum = 2
 
-//    //正誤判定の別パターン。
-//    //正誤判定の結果を格納する変数。
-//    var answer = true
+    //正誤判定の結果を格納する変数。
+    var answer = true
+
+    //Toastで表示する文字列。
+    var toastMessage: String = ""
+
+    //今何問目かを管理する変数。
+    var status = 0
+
+    //正解数をカウントする変数。
+    var correctCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +35,6 @@ class MainActivity : AppCompatActivity() {
 
         //最初の問題を生成。
         makeQuestion()
-
-//      //makeQuestion()の別パターン。
-//      questionNum = makeQuestion()
 
         //クリックリスナを設定。
         rightButton.setOnClickListener(JudgeListener())
@@ -40,40 +44,66 @@ class MainActivity : AppCompatActivity() {
     private inner class JudgeListener: View.OnClickListener {
         override fun onClick(view: View) {
 
+            //正誤判定。
+            judgeAnswer(questionNum)
+
             //ボタンのidに応じて処理を変える。
             when(view.id) {
                 R.id.rightButton -> {
-                    when(judgeAnswer(questionNum)) {
-                        true -> resultTextView.text = "正解！"
-                        false -> resultTextView.text = "間違い！"
+                    when (answer) {
+                        true -> {
+                            toastMessage = "正解！$toastMessage"
+                            correctCount += 1
+                        }
+                        false -> toastMessage = "間違い！$toastMessage"
                     }
-
-//                    //正誤判定の別パターン。
-//                    when(answer) {
-//                        true -> resultTextView.text = "Right !"
-//                        false -> resultTextView.text = "Wrong !"
-//                    }
                 }
 
                 R.id.wrongButton -> {
-                    when(judgeAnswer(questionNum)) {
-                        true -> resultTextView.text = "間違い！"
-                        false -> resultTextView.text = "正解！"
+                    when (answer) {
+                        true -> toastMessage = "間違い！$toastMessage"
+                        false -> {
+                            toastMessage = "正解！$toastMessage"
+                            correctCount += 1
+                        }
                     }
                 }
             }
 
-            //正誤に応じて処理を変える。
-            when(resultTextView.text) {
-                //文字色を変更。
-                "正解！" -> resultTextView.setTextColor(Color.parseColor("#4ada4a"))
-                "間違い！" -> resultTextView.setTextColor(Color.parseColor("#da4a4a"))
+            //何問目かによって処理を変える。
+            if (status == QUESTION_COUNT) {
+
+                //正誤を表示。
+                Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT).show()
+
+                //TextViewに結果を表示。
+                explanationTextView.text = "ボタンを押して再チャレンジ！"
+                questionNumTextView.text = "${correctCount}問正解！"
+
+                //statusを更新。
+                status = -1
+
+            } else if(status == -1) {
+                //リセット。
+                status = 0
+                correctCount = 0
+                explanationTextView.setText(R.string.explanatory_text)
+
+                //最初の問題を生成。
+                makeQuestion()
+
+            } else {
+
+                //正誤を表示。
+                Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT).show()
+
+                //次の問題を生成。
+                makeQuestion()
+
             }
 
-            //次の問題を生成。
-            makeQuestion()
-
         }
+
     }
 
     //問題を生成。
@@ -84,54 +114,40 @@ class MainActivity : AppCompatActivity() {
         //整数を表示。
         questionNumTextView.text = questionNum.toString()
 
+        //statusを更新。
+        status += 1
+
     }
 
-//    //makeQuestion()の別パターン。
-//    private fun makeQuestion(): Int{
-//        //ランダムに整数を決定。
-//        var questionNum = Random.nextInt(2, 100)
-//
-//        //整数を表示。
-//        questionNumTextView.text = questionNum.toString()
-//
-//        return questionNum
-//    }
-
     //正誤判定。
-    private fun judgeAnswer(questionNum: Int) :Boolean{
-        //正誤判定の結果を格納する変数。
-        var answer = true
-
+    private fun judgeAnswer(questionNum: Int) {
         //素数かどうか判定。
         for (i in 2 until questionNum) {
             if (questionNum%i == 0) {
+
+                //questionNumは素数ではないので、false。
                 answer = false
-                reasonTextView.text = "${questionNum}は${i}の倍数だ！"
-                Log.d("judge", "${questionNum}は${i}の倍数だ！")
+
+                toastMessage = "(${i}の倍数)"
+
+                //素数か否かの判定結果をログ出力。
+                Log.d("judge", "${questionNum}:${i}の倍数")
+
                 break
+
             } else if (i+1 == questionNum) {
-                reasonTextView.text = "${questionNum}は素数だ！"
-                Log.d("judge", "${questionNum}は素数だ！")
+
+                //questionNumは素数なので、true。
+                answer = true
+
+                toastMessage = "(素数)"
+
+                //素数か否かの判定結果をログ出力。
+                Log.d("judge", "${questionNum}:素数")
             }
         }
-
+        //正誤判定をログ出力。
         Log.d("judge", answer.toString())
-        return answer
-
     }
-
-//    //正誤判定の別パターン。
-//    private fun checkAnswer(questionNum: Int){
-//        //素数かどうか判定。
-//        for (i in 2 until questionNum-1) {
-//            if (questionNum%i == 0) {
-//                answer = false
-//                reasonTextView.text = "It's a multiple of ${i}"
-//                break
-//            } else {
-//                reasonTextView.text = "It's a prime number"
-//            }
-//        }
-//    }
 
 }
